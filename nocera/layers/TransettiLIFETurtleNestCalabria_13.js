32728308ff0d9422f537758cd36cce5081ca4db1
franco-caponi-ws <141813503+franco-caponi-ws@users.noreply.github.com>
2025-07-13 21:13:42 +0200

    file mancanti

---
+++ nocera/layers/TransettiLIFETurtleNestCalabria_13.js
@@ -0,0 +1,8 @@
+var json_TransettiLIFETurtleNestCalabria_13 = {
+"type": "FeatureCollection",
+"name": "TransettiLIFETurtleNestCalabria_13",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
